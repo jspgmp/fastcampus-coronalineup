ef55b507fd3280358af514b1ba94ef55e6ce5e91
JI <mmriann@gmail.com>
2022-01-12 15:15:13 +0900

    Add: SpringBootApplication

---
+++ src/main/java/com/fastcampus/getinline/CoronaLineApplication.java
@@ -0,0 +1,11 @@
+package com.fastcampus.getinline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaLineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaLineApplication.class, args);
+    }
+}
